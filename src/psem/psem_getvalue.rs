90bedd804270593a115d98572683e3fdff71ab01
lkitching <lee.kitching@gmail.com>
2021-10-17 21:17:19 +0100

    Implement listing 53-5
    Implement listing 53-5 which displays the value of a named POSIX
    semaphore.

---
+++ src/psem/psem_getvalue.rs
@@ -0,0 +1,25 @@
+// listing 53-5 (page 1098)
+use std::env;
+use std::os::raw::{c_int};
+
+use libc::{sem_getvalue};
+
+use rlpi::psem::sem_utils::open_existing;
+use rlpi::error_functions::{usage_err, err_exit};
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 || args[1] == "--help" {
+        usage_err(&format!("{} sem-name", args[0]));
+    }
+
+    let sem = open_existing(args[1].as_str()).unwrap();
+    
+    let mut value = 0;
+    if unsafe { sem_getvalue(sem, &mut value) } == -1 {
+        err_exit("sem_getvalue");
+    }
+
+    println!("{}", value);
+}
